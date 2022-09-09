2cc4e239e84e112bb074ef5803e4f5e60acd9a2f
deck <nicolaybang@gmail.com>
2022-09-09 12:20:46 -0400

    Added table PrepTime with a reference to it in Recipe (prep_time_id).

---
+++ 7_create_prep_time.sql
@@ -0,0 +1,5 @@
+CREATE TABLE PrepTime(
+    prep_time_id INT NOT NULL AUTO_INCREMENT,
+    prep_time VARCHAR(50) NOT NULL,
+    PRIMARY KEY(prep_time_id)
+)
--- 8_create_recipe.sql
+++ 8_create_recipe.sql
@@ -19,6 +19,7 @@ CREATE TABLE Recipe
     category_id   INT          NOT NULL,
     difficulty_id INT      DEFAULT NULL,
     title_id      INT          NOT NULL,
+    prep_time_id  INT          NOT NULL,
     date_created  DATETIME DEFAULT CURRENT_TIMESTAMP,
     description   VARCHAR(500) NOT NULL,
     PRIMARY KEY (recipe_id),
@@ -26,7 +27,8 @@ CREATE TABLE Recipe
     FOREIGN KEY (image_id) REFERENCES Image (image_id),
     FOREIGN KEY (category_id) REFERENCES Category (category_id),
     FOREIGN KEY (difficulty_id) REFERENCES Difficulty (difficulty_id),
-    FOREIGN KEY (title_id) REFERENCES Title (title_id)
+    FOREIGN KEY (title_id) REFERENCES Title (title_id),
+    FOREIGN KEY (prep_time_id) REFERENCES PrepTime (prep_time_id)
 
 );
 
