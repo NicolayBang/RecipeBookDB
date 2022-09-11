aebb540b18b111f629be2f80a56bd6e8a230f151
deck <nicolaybang@gmail.com>
2022-09-11 17:25:03 -0400

    Modified RecipeView to show pseudo instead of email. Also added a Unique constraint to pseudo.

--- CreateViews/15_create_recipe_view.sql
+++ CreateViews/15_create_recipe_view.sql
@@ -1,24 +1,16 @@
-
-
 CREATE View RecipeView AS
 (
-SELECT Title.title                                 AS title_view,
-       User.email                                  AS posted_by,
-       Recipe.date_created                         AS creation_date,
+SELECT Title.title           AS name,
+       User.pseudo           AS posted_by,
+       Recipe.date_created   AS creation_date,
        Difficulty.difficulty,
-       PrepTime.prep_time                          AS cook_time,
-       Recipe.nb_of_likes                          AS popularity,
-       Recipe.nb_of_servings                       AS servings,
+       PrepTime.prep_time    AS cook_time,
+       Recipe.nb_of_likes    AS popularity,
+       Recipe.nb_of_servings AS servings,
        Recipe.description
 
 FROM Recipe
-            INNER JOIN Title
-                    ON Title.title_id = Recipe.title_id
-            INNER JOIN User
-                    ON User.user_id = Recipe.user_id
-            INNER JOIN Difficulty
-                    ON Difficulty.difficulty_id = Recipe.difficulty_id
-            INNER JOIN PrepTime
-                    ON PrepTime.prep_time_id = Recipe.prep_time_id
-
-);
+         INNER JOIN Title ON Title.title_id = Recipe.title_id
+         INNER JOIN User ON User.user_id = Recipe.user_id
+         INNER JOIN Difficulty ON Difficulty.difficulty_id = Recipe.difficulty_id
+         INNER JOIN PrepTime ON PrepTime.prep_time_id = Recipe.prep_time_id );
