b53ced31327a9b2f2100e9822f8ac09afecdca44
deck <nicolaybang@gmail.com>
2022-09-12 15:15:11 -0400

    Created RecipeIngredientsView, works correctly. Need to create RecipeInstructionsView, then all the views ready for phase one. Other Views will be added in the future, like RecipeFavouritesView and RecipeFeaturedView.

--- CreateTables/12_create_ingredient.sql
+++ CreateTables/12_create_ingredient.sql
@@ -9,10 +9,10 @@ CREATE TABLE Ingredient
     ingredients_id     INT          NOT NULL,
     unit_id            INT          NOT NULL, # unit_id is a foreign key to the Unit table (cup, tablespoon, grams etc.)
     ingredient         TINYTEXT     NOT NULL,
-    quantity           int          NOT NULL,
+    quantity           int          NOT NULL
 
     #UNIQUE (ingredients_id, ingredient)
-    FOREIGN KEY (ingredients_id) REFERENCES Ingredients (ingredients_id)
+#    FOREIGN KEY (ingredients_id) REFERENCES Ingredients (ingredients_id)
 
 
 );
